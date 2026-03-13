d2c3b6cd26d9376ebd9e7ac37fe6319d5e245c26
Sanuka Waravita <sanukawaravita@gmail.com>
2026-03-13 14:00:40 +0530

    feat: add express dependency and implement basic server with user input

---
+++ server.js
@@ -0,0 +1,27 @@
+// port config
+const port = 3001;
+
+// express config
+const exp = require('express');
+const readline = require('readline');
+
+// config the server application
+const app = exp();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter your name: ', (name) => {
+    rl.question('Enter your batch number: ', (batchNumber) => {
+        console.log(`Name: ${name}`);
+        console.log(`Batch Number: ${batchNumber}`);
+
+        rl.close();
+
+        app.listen(port, () => {
+            console.log(`Server is under the water at port: ${port}`);
+        });
+    });
+});
